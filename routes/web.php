d5d44502eac4cb4442d5f31ed9f161a5edcdc2b4
Abdourahmane Diop <cabraldiop18@gmail.com>
2020-08-10 04:57:51 +0000

    feat: (laravel ui) make authencation system

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'AppController@index')->name('welcome');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
