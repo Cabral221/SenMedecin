7e9ed2483b201c636e05075830f89fddaa89cb90
Abdourahmane Diop <cabraldiop18@gmail.com>
2020-12-18 13:24:36 +0000

    Feat: (database migration) for message table and comment post

---
+++ database/migrations/2020_12_18_131723_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->string('email');
+            $table->text('comment');
+            $table->bigInteger('post_id')->unsigned()->index();
+            $table->timestamps();
+
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
